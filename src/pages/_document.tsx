587fe90f90d026b5917f605f96b1c26b3e289ba8
kingverite <eliassaoman@gmail.com>
2025-05-14 14:28:31 +0200

    commid 1F

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -3,8 +3,13 @@ import Header from '../components/Header';
 
 export default function Document() {
   return (
-    <Html lang="en">
-      <Head />
+    <Html lang="fr">
+      <Head>
+        {/* Balise meta viewport à ajouter ici */}
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        {/* Autres balises meta ou liens ici */}
+      </Head>
+
       <Header />
       <body className="antialiased">
         <Main />
